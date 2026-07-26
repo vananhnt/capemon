c3c1131f2efab989a46db37adcf4fa15aad35bbb
vananhnt <vananhnt@gmail.com>
2026-07-26 21:12:01 +0900

    chore: SAGE API generation api_gen_attempt_001 build 3
    Run-ID: run_20260726-111451_a66324
    
    API-Generation-Attempt: api_gen_attempt_001
    
    Experiment-Tag: claude_code_pa-alk_api_gen_attempt_001_run_20260726-111451_a66324
    
    Build-Attempt: 3
    
    Hook-Count: 16
    
    APIs: EnumPrintersA, FindFirstFileA, FindNextFileA, GetLastInputInfo, GetNativeSystemInfo, GetSystemInfo, GlobalMemoryStatusEx, PathFileExistsA, PathFileExistsW, Process32FirstW, Process32NextW, RegEnumKeyExA, RegQueryValueExA, SLIsGenuineLocal, WMI_ExecQuery, WMI_Next

--- hook_wmi.c
+++ hook_wmi.c
@@ -152,14 +152,10 @@ HOOKDEF(HRESULT, WINAPI, WMI_Next,
 )
 {
 HRESULT ret;
-	ULONG returned;
 
-	ret = Old_WMI_Next(_this, lTimeout, uCount, ppObjects, puReturned);
+	ret = Old_WMI_Next(_this, lFlags, strName, pVal, pType, plFlavor);
 
-	returned = (puReturned != NULL) ? *puReturned : 0;
-
-	LOQ_hresult("misc", "pliil", "This", _this, "Timeout", lTimeout,
-		"Count", uCount, "Returned", returned, "Objects", ppObjects);
+	LOQ_hresult("misc", "pl", "This", _this, "Flags", lFlags);
 
 	return ret;
 }
